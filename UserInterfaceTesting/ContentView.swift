f228f13ada26a92d14eb98f79eb3f3a9287119b0
Alessandro Negrão <alessandro.negrao.s@gmail.com>
2021-04-28 00:16:51 -0400

    Initial Commit

---
+++ UserInterfaceTesting/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  UserInterfaceTesting
+//
+//  Created by Alessandro Negrão on 28/04/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UserInterfaceTesting/UserInterfaceTestingApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserInterfaceTestingApp.swift
+//  UserInterfaceTesting
+//
+//  Created by Alessandro Negrão on 28/04/21.
+//
+
+import SwiftUI
+
+@main
+struct UserInterfaceTestingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
